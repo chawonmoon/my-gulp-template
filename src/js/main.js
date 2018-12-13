8c97616058953ac7fd84ba5cf76a6c89b1c56673
cchamang <cchamang@gmail.com>
2018-12-13 12:50:25 +0900

    Type: feat
    Default gulp setting
    html/css/js minify&bundling
    
    Next: Change es6 / Change scss

---
+++ src/js/main.js
@@ -0,0 +1,6 @@
+(function () {
+    var dummy1 = require('./modules/dummy1');
+    var dummy2 = require('./modules/dummy2');
+    dummy1();
+    dummy2();
+})();
---
+++ src/js/modules/dummy2.js
@@ -0,0 +1,6 @@
+// 더미 함수 2
+function dummy2 (){
+    console.log('world!! haha!!!!');
+}
+
+module.exports = dummy2;
